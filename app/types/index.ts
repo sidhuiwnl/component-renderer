e60359b03d0258382b1ae8f24a9110b012595005
Sidharth <sidharthinfernal@gmail.com>
2024-08-10 09:06:31 +0530

    added loading button

---
+++ app/types/index.ts
@@ -0,0 +1,10 @@
+export type VariantType =
+  | "default"
+  | "link"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost";
+
+
+export type SizeType = "default" | "sm" | "lg" | "icon";
--- components/optionsCard.tsx
+++ components/optionsCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
@@ -19,26 +19,32 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import { VariantType,SizeType } from "@/app/types";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
-type VariantType = "default" | "link" | "destructive" | "outline" | "secondary" | "ghost";
-type SizeType = "default" | "sm" | "lg" | "icon" ;
 
-export default function OptionsCard(){
-    const[text,setText] = useState("Click me");
-    const[variant,setVariant] = useState<VariantType>("default");
-    const[sizeOfBtn,setSizeOfBtn] = useState<SizeType>("default");
-    return(
-        <div className=" w-full h-screen flex flex-row justify-center items-center">
-            <Card className="w-[350px] mr-5">
+
+
+export default function OptionsCard() {
+  const [text, setText] = useState("Click me");
+  const [variant, setVariant] = useState<VariantType>("default");
+  const [sizeOfBtn, setSizeOfBtn] = useState<SizeType>("default");
+  const [loadingState, setLoadingState] = useState(false);
+  
+
+
+  
+  return (
+    <div className=" w-full h-screen flex flex-row justify-center items-center">
+      <Card className="w-[350px] mr-5">
         <CardHeader>
           <CardDescription className="text-white">Options</CardDescription>
-          
         </CardHeader>
         <CardContent>
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-              
                 <Select>
                   <SelectTrigger id="framework">
                     <SelectValue placeholder="Button" />
@@ -54,16 +60,22 @@ export default function OptionsCard(){
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="btn-text">Button text</Label>
-                <Input id="btn-text" defaultValue={"Click me"} onChange={(e) => setText(e.target.value)}/>
+                <Input
+                  id="btn-text"
+                  defaultValue={"Click me"}
+                  onChange={(e) => setText(e.target.value)}
+                />
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="framework">Variant</Label>
-                <Select onValueChange={(value : VariantType) => setVariant(value)}>
+                <Select
+                  onValueChange={(value: VariantType) => setVariant(value)}
+                >
                   <SelectTrigger id="framework">
                     <SelectValue placeholder="default" />
                   </SelectTrigger>
-                  <SelectContent position="popper" >
-                  <SelectItem value="default">default</SelectItem>
+                  <SelectContent position="popper">
+                    <SelectItem value="default">default</SelectItem>
                     <SelectItem value="destructive">destructive</SelectItem>
                     <SelectItem value="outline">outline</SelectItem>
                     <SelectItem value="secondary">secondary</SelectItem>
@@ -74,7 +86,9 @@ export default function OptionsCard(){
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="framework">Size</Label>
-                <Select onValueChange={(value : SizeType) => setSizeOfBtn(value)}>
+                <Select
+                  onValueChange={(value: SizeType) => setSizeOfBtn(value)}
+                >
                   <SelectTrigger id="framework">
                     <SelectValue placeholder="default" />
                   </SelectTrigger>
@@ -83,23 +97,50 @@ export default function OptionsCard(){
                     <SelectItem value="sm">sm</SelectItem>
                     <SelectItem value="lg">lg</SelectItem>
                     {/* <SelectItem value="icon">icon</SelectItem> */}
-                    
                   </SelectContent>
                 </Select>
               </div>
-             
+              <div className="flex space-x-2">
+              
+                <Checkbox id="terms"
+                checked={loadingState}
+                onCheckedChange={(checked) => setLoadingState(checked as boolean)}
+                />
+                <label
+                  htmlFor="terms"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Loading state
+                </label>
+              </div>
             </div>
           </form>
         </CardContent>
-       
       </Card>
       <Card className="w-[350px] h-[400px]">
         <div className="p-5 text-center">
-        <Button variant={variant} size={sizeOfBtn} >{text}</Button>
+        {loadingState ? <ButtonLoading/> : <DefaultButton variant={variant} text={text} sizeOfBtn={sizeOfBtn} />}
+          
         </div>
       </Card>
-      
-        </div>
-        
-    )
+    </div>
+  );
 }
+
+
+function ButtonLoading() {
+  return (
+    <Button disabled>
+      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+      Please wait
+    </Button>
+  )
+}
+
+ function DefaultButton({variant,text,sizeOfBtn} :{variant : VariantType,text : string, sizeOfBtn : SizeType}){
+  return(
+      <Button variant={variant} size={sizeOfBtn}>
+            {text}
+      </Button> 
+  )
+ }
